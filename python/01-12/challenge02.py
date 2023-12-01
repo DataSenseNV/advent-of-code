0f99fff13e34d9b8ae9e6038124caed7a367e708
BertSwerts <36440028+BertSwerts@users.noreply.github.com>
2023-12-01 11:17:53 +0100

    Day1

--- python/01-12/challenge02.py
+++ python/01-12/challenge02.py
@@ -2,16 +2,32 @@
 # https://adventofcode.com/2023/day/1
 
 numbers = "0123456789"
+dict = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 values = []
 
 with open("input.txt") as file:
     for item in file:
         first = ""
         last = ""
+        print(item)
+        for x in dict.keys():
+            if x in item:
+                item = item.replace(x, x + dict[x] + x)
+        print(item)
         for char in item:
             if char in numbers:
                 if first == "":
                     first = char
                 last = char
         values.append(int(first + last))
-print(sum(values))
+        print(sum(values))

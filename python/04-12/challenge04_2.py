1cfda9fbe368f3528a976ee35e97b10769347631
Jim Vekemans <Jim.Vekemans@datasense.be>
2023-12-04 23:34:57 +0100

    Increased dankness 🤓

--- python/04-12/challenge04_2.py
+++ python/04-12/challenge04_2.py
@@ -1,11 +1,7 @@
 import re
-
-with open("input.txt", "r") as f:
-    lines = f.readlines()
-
-card_amounts, card_matches = dict((f"Card {i+1}", 1) for i in range(len(lines))), {}
-for index, line in enumerate(lines):
-    card_matches[f"Card {index+1}"] = len(set([int(v) for v in re.findall("\d+", line.split("|")[0].split(":")[1])]).intersection(set([int(v) for v in re.findall("\d+", line.split("|")[1])])))
-    for next_up in range(card_matches[f"Card {index+1}"]):
-        card_amounts[f"Card {index+next_up+2}"] += card_amounts[f"Card {index+1}"] if index+next_up+2 <= len(lines) else 0
-print(sum(card_amounts.values()))
+ls = open("input.txt", "r").readlines()
+x = dict((f"Card {i+1}", 1) for i in range(len(ls)))
+for i, l in enumerate(ls):
+    for j in range(len(set([int(v) for v in re.findall("\d+", l.split("|")[0].split(":")[1])]).intersection(set([int(v) for v in re.findall("\d+", l.split("|")[1])])))):
+        x[f"Card {i+j+2}"] += x[f"Card {i+1}"] if i+j+2 <= len(ls) else 0
+print(sum(x.values()))

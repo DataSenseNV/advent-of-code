5f114f8dcd5ad79af3634a2738299d8845de0a40
BertSwerts <36440028+BertSwerts@users.noreply.github.com>
2023-12-04 17:04:44 +0100

    Day 4_2

--- python/04-12/challenge04_2.py
+++ python/04-12/challenge04_2.py
@@ -10,16 +10,15 @@ with open("input.txt") as file:
         amount.append(1)
 with open("input.txt") as file:
     for line in file:
-        for times in range(amount[current_number]):
-            matching = 0
-            data = line.strip().split(":")
-            winning = list(filter(None, data[1].split("|")[0].split(" ")))
-            owned = list(filter(None, data[1].split("|")[1].split(" ")))
-            for number in owned:
-                if number in winning:
-                    matching += 1
-            for x in range(matching):
-                amount[current_number + x + 1] += 1
+        matching = 0
+        data = line.strip().split(":")
+        winning = list(filter(None, data[1].split("|")[0].split(" ")))
+        owned = list(filter(None, data[1].split("|")[1].split(" ")))
+        for number in owned:
+            if number in winning:
+                matching += 1
+        for x in range(matching):
+            amount[current_number + x + 1] += amount[current_number]
 
         current_number += 1
 print(sum(amount))

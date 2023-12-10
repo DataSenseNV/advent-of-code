ec44670d47ff97cde0e9d54c9ce7e06bd9d7f16b
Jim Vekemans <JimVekemans@hotmail.com>
2023-12-10 19:19:13 +0100

    Kakoefening laat me nadenken in weekend, smh

--- python/08-12/challenge08.py
+++ python/08-12/challenge08.py
@@ -1,25 +1,24 @@
 # Day 8 - Advent of Code 2023
 # https://adventofcode.com/2023/day/8
 
+from math import lcm
+
 with open("python/08-12/input.txt", "r") as file:
     lines = [line.strip() for line in file.readlines()]
 
+nodes = dict((line[:3], {"L": line[7:10], "R": line[-4:-1]}) for line in lines[2:])
 
-def part_1(instructions, nodes, current_node_name, amount_of_steps=0):
-    while current_node_name != "ZZZ":
+
+def get_steps_to_Z(starting_node, amount_of_steps=0):
+    instructions = list(lines[0])
+    while not starting_node.endswith("Z"):
         instruction = instructions.pop(0)
-        current_node_name = nodes.get(current_node_name).get(instruction)
-        amount_of_steps += 1
+        starting_node = nodes.get(starting_node).get(instruction)
         instructions.append(instruction)
+        amount_of_steps += 1
     return amount_of_steps
 
 
-if __name__ == "__main__":
-    part_1 = part_1(
-        instructions=list(lines[0]),
-        current_node_name=lines[2][:3],
-        nodes=dict(
-            (line[:3], {"L": line[7:10], "R": line[-4:-1]}) for line in lines[2:]
-        ),
-    )
-    print(part_1)
+print("Part 1:", get_steps_to_Z("AAA"))
+len_to_Z = {node: get_steps_to_Z(node) for node in nodes if node.endswith("A")}
+print("Part 2:", lcm(*len_to_Z.values()))
